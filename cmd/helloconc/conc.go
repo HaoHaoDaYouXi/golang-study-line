cd836f98e741c917d135e9baad3ab844236cae72
ShaoBo Wan <overcome.wan@gmail.com>
2018-03-04 21:39:56 +0800

    go starts a goroutine 其他语言通过异步IO来完成并发

--- cmd/helloconc/conc.go
+++ cmd/helloconc/conc.go
@@ -2,20 +2,29 @@
 package main
 
 import (
-	"time"
 	"fmt"
+	"time"
 )
 
-func main () {
-	// go starts a goroutine
-	for i:=0; i<5000; i++ {
+func main() {
+	// 通道
+	ch := make(chan string)
+	// go starts a goroutine 其他语言通过异步IO来完成并发
+	for i := 0; i < 5000; i++ {
+		// 并发
 		go printHello(i)
 		time.Sleep(time.Millisecond)
 	}
+
+	//通信
+	for {
+		msg := <-ch
+		fmt.Println(msg)
+	}
 }
 
-func printHello(i int){
+func printHello(i int) {
 	for {
-		fmt.Printf("Hello World from goroutine %d\n",i)
+		fmt.Printf("Hello World from goroutine %d\n", i)
 	}
 }
